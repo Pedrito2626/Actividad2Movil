18d0d51dad467cab8ddd075956f4b614e3ee67b4
Pedrito2626 <esteban7976@outlook.com>
2026-09-19 07:51:08 -0500

    Func Completitud de la pantalla login

--- lib/pantalla/login.dart
+++ lib/pantalla/login.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
-class PantallaLogin extends StatelessWidget {
+class PantallaLogin extends StatefulWidget {
   const PantallaLogin({super.key});
+
+  @override
+  State<PantallaLogin> createState() => _PantallaLoginState();
+}
+class _PantallaLoginState extends State<PantallaLogin> {
+  final _correoController = TextEditingController();
+  final _contrasenaController = TextEditingController();
+
+  @override
+  void dispose() {
+    _correoController.dispose();
+    _contrasenaController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
-      body: const Center(child: Text('Login Page')),
-    );
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              controller: _correoController,
+              decoration: InputDecoration(
+                labelText: 'Correo',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller: _contrasenaController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Contraseña',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => PantallaLogin(
+                    correo: _correoController.text,
+                    contrasena: _contrasenaController.text,
+                  )),
+                );
+              },
+              child:  Text('Iniciar Sesión'),
+            ),
+            
+          ],
+        ),
+      ),
+    );  
   }
 }
